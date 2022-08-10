62cbda1227aa7f5488b9e2040a2fb613bc5c4a13
DenysSlota <107800499+DenysSlota@users.noreply.github.com>
2022-08-10 16:00:33 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,17 @@
+"use strict"
+
+const rewiewsSwiper = document.querySelector('.swiper-rewiews');
+
+if (rewiewsSwiper) {
+    const swiper = new Swiper('.swiper-rewiews', {
+        // Optional parameters
+        autoHeight: true,
+        loop: true,
+    
+        // If we need pagination
+        pagination: {
+        el: '.swiper-pagination',
+        clickable: true
+        },
+    });
+}
